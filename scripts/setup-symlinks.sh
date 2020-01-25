bd6bae2e8b124705bac2c207c3ce5333893d8848
antonstrand <jag@antonstrand.se>
2020-01-25 11:53:13 +0100

    Adding settings for tmux

--- scripts/setup-symlinks.sh
+++ scripts/setup-symlinks.sh
@@ -7,6 +7,8 @@ ln -nfs ~/.dotfiles/.zshrc ~/.zshrc
 ln -nfs ~/.dotfiles/.gitconfig ~/.gitconfig
 touch ~/.vimrc
 ln -nfs ~/.dotfiles/.vimrc ~/.vimrc
+touch ~/.tmux.conf
+ln -nfs ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # Visual Studio Code
 ln -nfs ~/.dotfiles/VSCode/* ~/Library/Application\ Support/Code/User/
