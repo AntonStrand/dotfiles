bf9150a5918fcdeab555bd375d423c886a1e8f31
Anton Strand <anton.strand@insurello.se>
2024-01-23 09:33:08 +0100

    Clean up keymaps

--- nvim/lua/antonstrand/core/keymaps.lua
+++ nvim/lua/antonstrand/core/keymaps.lua
@@ -1,15 +1,16 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.keymap.set('n', '<leader>sf', '/', { desc = "Search forwards" })
-vim.keymap.set('n', '<leader>sb', '?', { desc = "Search backwards" })
-vim.keymap.set('n', '<leader>sh', ':nohlsearch<CR>', { desc = "Hide search results" })
+vim.keymap.set("n", "<leader>sh", ":nohlsearch<CR>", { desc = "Hide search results" })
+
+-- Re-map common typo
+vim.keymap.set("n", ":'", ":w", { desc = "Write file" })
 
 -- Move and center
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '}', '}zz')
-vim.keymap.set('n', '{', '{zz')
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "}", "}zz")
+vim.keymap.set("n", "{", "{zz")
 
 -- Generate uuid
-vim.keymap.set('n', '<leader>ug', ':!uuidgen | pbcopy<CR><CR>')
+vim.keymap.set("n", "<leader>ug", ":!uuidgen | pbcopy<CR><CR>")
