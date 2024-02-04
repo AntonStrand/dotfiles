5ebf2d875d3af2d727dd500112d8620d5da53d0c
Anton Strand <anton.strand@insurello.se>
2024-02-04 22:12:02 +0100

    Add highlight to yanked text

---
+++ nvim/lua/antonstrand/core/highligt_yank.lua
@@ -0,0 +1,8 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight selection on yank",
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ visual = true, timeout = 200 })
+	end,
+})
--- nvim/lua/antonstrand/core/init.lua
+++ nvim/lua/antonstrand/core/init.lua
@@ -1,3 +1,4 @@
 require("antonstrand.core.settings")
 require("antonstrand.core.keymaps")
 require("antonstrand.core.fsharp")
+require("antonstrand.core.highligt_yank")
