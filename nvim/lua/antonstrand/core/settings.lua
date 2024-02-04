84406f3c224f25954c453c74177c092676c9fbc3
Anton Strand <anton.strand@insurello.se>
2024-02-04 22:16:07 +0100

    Enable 24-bit color

--- nvim/lua/antonstrand/core/settings.lua
+++ nvim/lua/antonstrand/core/settings.lua
@@ -25,5 +25,8 @@ vim.opt.completeopt = { "menuone", "noselect" }
 -- Enable persistent undo history
 vim.opt.undofile = true
 
+-- Enable 24-bit color
+vim.opt.termguicolors = true
+
 -- Enable copy/paste to clipboard
 vim.cmd([[ set clipboard+=unnamedplus ]])
