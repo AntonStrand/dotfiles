227219f7519be899d1896141ace49c8e695b5797
Anton Strand <anton.strand@insurello.se>
2024-02-04 22:28:34 +0100

    Open as a popup

--- nvim/lua/antonstrand/plugins/oil.lua
+++ nvim/lua/antonstrand/plugins/oil.lua
@@ -1,10 +1,10 @@
 return {
-	"stevearc/oil.nvim",
-	opts = {},
-	-- Optional dependencies
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("oil").setup({})
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-	end,
+  "stevearc/oil.nvim",
+  opts = {},
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({})
+    vim.keymap.set("n", "<leader>e", require("oil").toggle_float, { desc = "Open [e]xplorer popup" })
+  end,
 }
