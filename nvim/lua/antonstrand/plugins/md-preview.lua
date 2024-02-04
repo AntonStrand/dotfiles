28f46cf29de44bad4dd83f6c1c9b90ce4a08e4eb
Anton Strand <anton.strand@insurello.se>
2024-02-04 22:26:12 +0100

    Remove shortcuts

--- nvim/lua/antonstrand/plugins/md-preview.lua
+++ nvim/lua/antonstrand/plugins/md-preview.lua
@@ -5,8 +5,4 @@ return {
   build = function()
     vim.fn["mkdp#util#install"]()
   end,
-  config = function()
-    vim.keymap.set("n", "<leader>mps", ":MarkdownPreview<CR>", { desc = "Start markdown preview" })
-    vim.keymap.set("n", "<leader>mpx", ":MarkdownPreviewStop<CR>", { desc = "Stop markdown preview" })
-  end,
 }
