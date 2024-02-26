7278312eec7118e2d7888dcf467d4de442fa753c
Anton Strand <anton.strand@insurello.se>
2024-02-26 09:28:25 +0100

    Quickly move current line(s) in visual mode

--- nvim/lua/antonstrand/core/keymaps.lua
+++ nvim/lua/antonstrand/core/keymaps.lua
@@ -23,3 +23,7 @@ vim.keymap.set("n", "#", "#zz")
 
 -- Generate uuid
 vim.keymap.set("n", "<leader>ug", ":!uuidgen | pbcopy<CR><CR>", { desc = "Generate UUID" })
+
+-- Move selected lines with shift+j or shift+k
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
