21dcf609a2648a6434f67f10708342de8d1d3d73
Anton Strand <anton.strand@insurello.se>
2024-03-01 23:41:47 +0100

    Add DB GUI

---
+++ nvim/lua/antonstrand/plugins/db.lua
@@ -0,0 +1,20 @@
+vim.api.nvim_create_user_command("OpenDB", ":tabnew | DBUI", { nargs = 0 })
+
+return {
+	"kristijanhusak/vim-dadbod-ui",
+	dependencies = {
+		{ "tpope/vim-dadbod", lazy = true },
+		{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		{ "catppuccin/nvim" },
+	},
+	cmd = {
+		"DBUI",
+		"DBUIToggle",
+		"DBUIAddConnection",
+		"DBUIFindBuffer",
+	},
+	init = function()
+		-- Your DBUI configuration
+		vim.g.db_ui_use_nerd_fonts = 1
+	end,
+}
