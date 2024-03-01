d4d60677225d387107c580d74147eef9fabcfea2
Anton Strand <anton.strand@insurello.se>
2024-03-01 23:43:14 +0100

    Use the same diagnostic icons as lualine does

--- nvim/lua/antonstrand/plugins/lsp/lpsconfig.lua
+++ nvim/lua/antonstrand/plugins/lsp/lpsconfig.lua
@@ -77,8 +77,8 @@ return {
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter)
-		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		-- Change the Diagnostic symbols in the sign column (gutter) to be the same as the ones used by lualine
+		local signs = { Error = "󰅚 ", Warn = "󰀪 ", Info = "󰋽 ", Hint = "󰌶 " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
