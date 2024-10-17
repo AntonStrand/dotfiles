9f7cedcf4de0a07396c0b3aea1a0b6d94daac5bb
Anton Strand <anton.strand@insurello.se>
2024-10-17 13:41:13 +0200

    Format

--- nvim/lua/antonstrand/core/switch_case.lua
+++ nvim/lua/antonstrand/core/switch_case.lua
@@ -2,74 +2,74 @@
 -- Inspired by https://dev.to/dimaportenko/switching-between-camelcase-and-snakecase-in-neovim-using-lua-3ah7
 
 local function replace_word(new_word)
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
-	local word_end = word_start + #vim.fn.expand("<cword>")
-	vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_end, { new_word })
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
+  local word_end = word_start + #vim.fn.expand("<cword>")
+  vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_end, { new_word })
 end
 
 local function snake_case_to_camel(word)
-	return word:gsub("(_)([a-z])", function(_, l)
-		return l:upper()
-	end)
+  return word:gsub("(_)([a-z])", function(_, l)
+    return l:upper()
+  end)
 end
 
 -- Can convert both Pascal and camelCase to snake_case
 local function to_snake_case(word)
-  return word:gsub('([a-z])([A-Z])', '%1_%2'):lower()
+  return word:gsub("([a-z])([A-Z])", "%1_%2"):lower()
 end
 
 local function first_upper(word)
-	return word:sub(1, 1):upper() .. word:sub(2)
+  return word:sub(1, 1):upper() .. word:sub(2)
 end
 
 local function is_camel_case(word)
-	return word:find("[a-z][A-Z]")
+  return word:find("[a-z][A-Z]")
 end
 
 local function is_pascal_case(word)
-	return word:find("^[A-Z]")
+  return word:find("^[A-Z]")
 end
 
 local function is_snake_case(word)
-	return word:find("_[a-z]")
+  return word:find("_[a-z]")
 end
 
 local function camel_case()
-	local word = vim.fn.expand("<cword>")
+  local word = vim.fn.expand("<cword>")
 
-	if is_pascal_case(word) then
-		-- Convert PascalCase to camelCase
-		local camel_case_word = word:sub(1, 1):lower() .. word:sub(2)
-		replace_word(camel_case_word)
-	elseif is_snake_case(word) then
-		replace_word(snake_case_to_camel(word))
-	else
-		print("Unable to convert to camelCase")
-	end
+  if is_pascal_case(word) then
+    -- Convert PascalCase to camelCase
+    local camel_case_word = word:sub(1, 1):lower() .. word:sub(2)
+    replace_word(camel_case_word)
+  elseif is_snake_case(word) then
+    replace_word(snake_case_to_camel(word))
+  else
+    print("Unable to convert to camelCase")
+  end
 end
 
 local function pascal_case()
-	local word = vim.fn.expand("<cword>")
+  local word = vim.fn.expand("<cword>")
 
-	if is_camel_case(word) then
-		replace_word(first_upper(word))
-	elseif is_snake_case(word) then
-		local camel_case_word = snake_case_to_camel(word)
-		replace_word(first_upper(camel_case_word))
-	else
-		print("Unable to convert to PascalCase")
-	end
+  if is_camel_case(word) then
+    replace_word(first_upper(word))
+  elseif is_snake_case(word) then
+    local camel_case_word = snake_case_to_camel(word)
+    replace_word(first_upper(camel_case_word))
+  else
+    print("Unable to convert to PascalCase")
+  end
 end
 
 local function snake_case()
-	local word = vim.fn.expand("<cword>")
+  local word = vim.fn.expand("<cword>")
 
-	if is_camel_case(word) or is_pascal_case(word) then
-		replace_word(to_snake_case(word))
-	else
-		print("Unable to convert to snake_case")
-	end
+  if is_camel_case(word) or is_pascal_case(word) then
+    replace_word(to_snake_case(word))
+  else
+    print("Unable to convert to snake_case")
+  end
 end
 
 vim.api.nvim_create_user_command("ToCamelCase", camel_case, {})
