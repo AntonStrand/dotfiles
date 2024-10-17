896c9705bbc5bbbe80799101facb71da2ea3a7e0
Anton Strand <anton.strand@insurello.se>
2024-10-17 13:45:34 +0200

    Change how to send to quicklist

--- nvim/lua/antonstrand/plugins/telescope.lua
+++ nvim/lua/antonstrand/plugins/telescope.lua
@@ -38,7 +38,7 @@ return {
 					i = { -- Insert mode
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 					},
 				},
 			},
