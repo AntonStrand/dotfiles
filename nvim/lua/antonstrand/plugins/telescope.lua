fc92facdff7474f9d63cd6ef69f1b169550a9228
Anton Strand <anton.strand@insurello.se>
2024-10-17 13:46:03 +0200

    Add n and p as valid scroll keys

--- nvim/lua/antonstrand/plugins/telescope.lua
+++ nvim/lua/antonstrand/plugins/telescope.lua
@@ -37,7 +37,9 @@ return {
 				mappings = {
 					i = { -- Insert mode
 						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-p>"] = actions.move_selection_previous, -- move to prev result
 						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-n>"] = actions.move_selection_next, -- move to next result
 						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
 					},
 				},
