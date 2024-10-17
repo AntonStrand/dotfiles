ed058c6e990c579ab507a7c8f8d0e88f3871e871
Anton Strand <anton.strand@insurello.se>
2024-10-17 13:52:27 +0200

    Set keymap on loaded

--- nvim/lua/antonstrand/plugins/zen-mode.lua
+++ nvim/lua/antonstrand/plugins/zen-mode.lua
@@ -1,5 +1,7 @@
-local zen_mode = require("zen-mode")
-
-vim.keymap.set("n", "<leader>z", zen_mode.toggle, { noremap = true, silent = true, desc = "Toggle Zen Mode" })
-
-return { "folke/zen-mode.nvim" }
+return {
+  "folke/zen-mode.nvim",
+  config = function()
+    local zen_mode = require("zen-mode")
+    vim.keymap.set("n", "<leader>z", zen_mode.toggle, { noremap = true, silent = true, desc = "Toggle Zen Mode" })
+  end,
+}
