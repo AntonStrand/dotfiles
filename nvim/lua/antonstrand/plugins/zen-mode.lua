6abbf02f36406c647acc650bafb8cf7ecaab9d32
Anton Strand <anton.strand@insurello.se>
2024-10-17 13:42:44 +0200

    Delete unused plugins

--- nvim/lua/antonstrand/plugins/zen-mode.lua
+++ nvim/lua/antonstrand/plugins/zen-mode.lua
@@ -1,10 +1,5 @@
-local total_zen = function()
-  require("twilight").toggle()
-	require("zen-mode").toggle()
-end
-vim.keymap.set("n", "<leader>z", total_zen, { noremap = true, silent = true, desc = "Toggle Zen Mode" })
+local zen_mode = require("zen-mode")
 
-return {
-	"folke/zen-mode.nvim",
-	"folke/twilight.nvim",
-}
+vim.keymap.set("n", "<leader>z", zen_mode.toggle, { noremap = true, silent = true, desc = "Toggle Zen Mode" })
+
+return { "folke/zen-mode.nvim" }
