c49789c46d04e0684a4e164097b3d2f7e436d275
Anton Strand <anton.strand@insurello.se>
2024-10-17 13:40:42 +0200

    Check for unnecessary parentheses in F#

--- nvim/lua/antonstrand/core/fsharp.lua
+++ nvim/lua/antonstrand/core/fsharp.lua
@@ -4,6 +4,7 @@ local vim = vim
 vim.g["fsharp#show_signature_on_cursor_move"] = 0
 vim.g["fsharp#lsp_auto_setup"] = 0
 vim.g["fsharp#workspace_mode_peek_deep_level"] = 4
+vim.g["fsharp#unnecessary_parentheses_analyzer"] = 1
 
 vim.api.nvim_create_user_command("FSharpRefreshCodeLens", function()
 	vim.lsp.codelens.refresh()
