d2b9b022777951ee1cea9bab3d54914aa501185a
Anton Strand <anton.strand@insurello.se>
2024-11-13 14:50:35 +0100

    Prioritize toggle bools under the cursor

--- nvim/lua/antonstrand/core/keymaps.lua
+++ nvim/lua/antonstrand/core/keymaps.lua
@@ -33,10 +33,30 @@ vim.keymap.set(
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+local function replace_word(new_word)
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
+	local word_end = word_start + #vim.fn.expand("<cword>")
+	vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_end, { new_word })
+end
+
 -- Toggle boolean
 vim.keymap.set("n", "<leader>tb", function()
 	local line = vim.api.nvim_get_current_line()
-	if line:match("true") then
+	local word = vim.fn.expand("<cword>")
+
+	if word == "true" then
+		replace_word("false")
+	elseif word == "false" then
+		replace_word("true")
+	-- Also support Elm's True and False
+	elseif word == "True" then
+		replace_word("False")
+	elseif word == "False" then
+		replace_word("True")
+
+	-- Replace all occurrences of true and false on a line
+	elseif line:match("true") then
 		local new_line = line:gsub("true", "false")
 		vim.api.nvim_set_current_line(new_line)
 	elseif line:match("false") then
