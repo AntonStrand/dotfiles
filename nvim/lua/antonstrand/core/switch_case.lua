b39d89b28f2b9d757268afb41ff8b4dbad3da157
Anton Strand <anton.strand@insurello.se>
2024-11-18 17:07:46 +0100

    Support visual selection and sentence case

--- nvim/lua/antonstrand/core/switch_case.lua
+++ nvim/lua/antonstrand/core/switch_case.lua
@@ -1,91 +1,77 @@
 -- A set of functions to switch between different casings
 -- Inspired by https://dev.to/dimaportenko/switching-between-camelcase-and-snakecase-in-neovim-using-lua-3ah7
 
-local function replace_word(new_word)
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
-	local word_end = word_start + #vim.fn.expand("<cword>")
-	vim.api.nvim_buf_set_text(0, line - 1, word_start, line - 1, word_end, { new_word })
+local function to_camel_case(selection)
+  local word = selection:gsub("([%w%d])%s([%w%d])", "%1_%2"):gsub("(_)([a-z%d])", function(_, l)
+    return l:upper()
+  end)
+
+  -- Make sure that the first char is lower case
+  return word:sub(1, 1):lower() .. word:sub(2)
 end
 
-local function snake_case_to_camel(word)
-	return word:gsub("(_)([a-z])", function(_, l)
-		return l:upper()
-	end)
+local function to_pascal_case(selection)
+  local camel = to_camel_case(selection)
+  return camel:sub(1, 1):upper() .. camel:sub(2)
 end
 
--- Can convert both Pascal and camelCase to snake_case
-local function to_snake_case(word)
-	return word:gsub("([a-z])([A-Z])", "%1_%2"):lower()
+local function to_snake_case(selection)
+  return to_camel_case(selection):gsub("([a-z%d])([A-Z%d])", "%1_%2"):lower()
 end
 
-local function to_sentence_case(word)
-	return word:gsub("([a-z])([A-Z])", "%1 %2"):lower()
+local function to_sentence_case(selection)
+  return to_camel_case(selection):gsub("([a-z%d])([A-Z%d])", "%1 %2"):lower()
 end
 
-local function first_upper(word)
-	return word:sub(1, 1):upper() .. word:sub(2)
+-- Sources
+-- https://github.com/theHamsta/nvim-treesitter/blob/a5f2970d7af947c066fb65aef2220335008242b7/lua/nvim-treesitter/incremental_selection.lua#L22-L30
+-- https://github.com/neovim/neovim/discussions/26092
+local function visual_selection_range()
+  -- If we don't change to normal mode the previous selection will be used.
+  vim.cmd([[ execute "normal! \<ESC>" ]])
+  local _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
+  local _, cerow, cecol, _ = unpack(vim.fn.getpos("'>"))
+  if csrow < cerow or (csrow == cerow and cscol <= cecol) then
+    return csrow - 1, cscol - 1, cerow - 1, cecol
+  else
+    return cerow - 1, cecol - 1, csrow - 1, cscol
+  end
 end
 
-local function is_camel_case(word)
-	return word:find("[a-z][A-Z]")
+local function set_text(line, start_pos, end_pos, text)
+  vim.api.nvim_buf_set_text(0, line, start_pos, line, end_pos, { text })
 end
 
-local function is_pascal_case(word)
-	return word:find("^[A-Z]")
-end
+local function transform(transformer)
+  local mode = vim.api.nvim_get_mode().mode
 
-local function is_snake_case(word)
-	return word:find("_[a-z]")
+  if mode == "n" then
+    local word = vim.fn.expand("<cword>")
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    local word_start = vim.fn.matchstrpos(vim.fn.getline("."), "\\k*\\%" .. (col + 1) .. "c\\k*")[2]
+    local word_end = word_start + #vim.fn.expand("<cword>")
+    set_text(line - 1, word_start, word_end, transform(word))
+  elseif mode == "v" then
+    local line, start_pos, _, end_pos = visual_selection_range()
+    local selection = unpack(vim.api.nvim_buf_get_text(0, line, start_pos, line, end_pos, {}))
+    set_text(line, start_pos, end_pos, transformer(selection))
+  end
 end
 
 local function camel_case()
-	local word = vim.fn.expand("<cword>")
-
-	if is_pascal_case(word) then
-		-- Convert PascalCase to camelCase
-		local camel_case_word = word:sub(1, 1):lower() .. word:sub(2)
-		replace_word(camel_case_word)
-	elseif is_snake_case(word) then
-		replace_word(snake_case_to_camel(word))
-	else
-		print("Unable to convert to camelCase")
-	end
+  transform(to_camel_case)
 end
 
 local function pascal_case()
-	local word = vim.fn.expand("<cword>")
-
-	if is_camel_case(word) then
-		replace_word(first_upper(word))
-	elseif is_snake_case(word) then
-		local camel_case_word = snake_case_to_camel(word)
-		replace_word(first_upper(camel_case_word))
-	else
-		print("Unable to convert to PascalCase")
-	end
+  transform(to_pascal_case)
 end
 
 local function snake_case()
-	local word = vim.fn.expand("<cword>")
-
-	if is_camel_case(word) or is_pascal_case(word) then
-		replace_word(to_snake_case(word))
-	else
-		print("Unable to convert to snake_case")
-	end
+  transform(to_snake_case)
 end
 
 local function sentence_case()
-	local word = vim.fn.expand("<cword>")
-
-	if is_camel_case(word) or is_pascal_case(word) then
-		replace_word(to_sentence_case(word))
-	elseif is_snake_case(word) then
-		replace_word(to_sentence_case(snake_case_to_camel(word)))
-	else
-		print("Unable to convert to sentence")
-	end
+  transform(to_sentence_case)
 end
 
 vim.api.nvim_create_user_command("ToCamelCase", camel_case, {})
@@ -93,7 +79,8 @@ vim.api.nvim_create_user_command("ToPascalCase", pascal_case, {})
 vim.api.nvim_create_user_command("ToSnakeCase", snake_case, {})
 vim.api.nvim_create_user_command("ToSentenceCase", sentence_case, {})
 
-vim.keymap.set("n", "<leader>tc", camel_case, { noremap = true, silent = true, desc = "Convert to camelCase" })
-vim.keymap.set("n", "<leader>tp", pascal_case, { noremap = true, silent = true, desc = "Convert to PascalCase" })
-vim.keymap.set("n", "<leader>ts", snake_case, { noremap = true, silent = true, desc = "Convert to snake_case" })
-vim.keymap.set("n", "<leader>tm", sentence_case, { noremap = true, silent = true, desc = "Convert to sentence" })
+local modes = { "n", "v" }
+vim.keymap.set(modes, "<leader>tc", camel_case, { noremap = true, silent = true, desc = "Convert to camelCase" })
+vim.keymap.set(modes, "<leader>tp", pascal_case, { noremap = true, silent = true, desc = "Convert to PascalCase" })
+vim.keymap.set(modes, "<leader>ts", snake_case, { noremap = true, silent = true, desc = "Convert to snake_case" })
+vim.keymap.set(modes, "<leader>tm", sentence_case, { noremap = true, silent = true, desc = "Convert to sentence" })
