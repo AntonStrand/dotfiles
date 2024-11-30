85e6166ac5856d37f5483386bd625a00e3518966
Anton Strand <anton.strand@insurello.se>
2024-11-30 22:55:50 +0100

    Add filetype specific settings

---
+++ nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,4 @@
+-- Using `vim.cmd` instead of `vim.wo` because it is yet more reliable
+vim.cmd("setlocal spell")
+vim.cmd("setlocal tw=50") -- This is the recommended summary length
+vim.cmd("setlocal wrap")
---
+++ nvim/after/ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+-- Using `vim.cmd` instead of `vim.wo` because it is yet more reliable
+vim.cmd("setlocal spell")
+vim.cmd("setlocal tw=80")
+vim.cmd("setlocal wrap")
