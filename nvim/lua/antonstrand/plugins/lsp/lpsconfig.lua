94312c5ec56df53f45c86e80791a6e8ee511e3ec
Anton Strand <anton.strand@insurello.se>
2024-12-04 13:08:51 +0100

    Add Gleam LSP

--- nvim/lua/antonstrand/plugins/lsp/lpsconfig.lua
+++ nvim/lua/antonstrand/plugins/lsp/lpsconfig.lua
@@ -1,208 +1,209 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		-- Neovim LSP
-		{ "folke/neodev.nvim", opts = {} },
-	},
-	config = function()
-		local lspconfig = require("lspconfig")
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    -- Neovim LSP
+    { "folke/neodev.nvim",                   opts = {} },
+  },
+  config = function()
+    local lspconfig = require("lspconfig")
 
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		local keymap = vim.keymap -- for conciseness
+    local keymap = vim.keymap -- for conciseness
 
-		local opts = { noremap = true, silent = true }
-		local on_attach = function(_, bufnr)
-			opts.buffer = bufnr
+    local opts = { noremap = true, silent = true }
+    local on_attach = function(_, bufnr)
+      opts.buffer = bufnr
 
-			-- set keybinds
-			opts.desc = "Show LSP references"
-			keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+      -- set keybinds
+      opts.desc = "Show LSP references"
+      keymap.set("n", "gr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-			opts.desc = "Go to declaration"
-			keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+      opts.desc = "Go to declaration"
+      keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-			opts.desc = "Show LSP definitions"
-			keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+      opts.desc = "Show LSP definitions"
+      keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
-			opts.desc = "Show LSP implementations"
-			keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+      opts.desc = "Show LSP implementations"
+      keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-			opts.desc = "Show LSP type definitions"
-			keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+      opts.desc = "Show LSP type definitions"
+      keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-			opts.desc = "See available code actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+      opts.desc = "See available code actions"
+      keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+      opts.desc = "Smart rename"
+      keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+      opts.desc = "Show buffer diagnostics"
+      keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
-			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+      opts.desc = "Show line diagnostics"
+      keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+      opts.desc = "Go to previous diagnostic"
+      keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+      opts.desc = "Go to next diagnostic"
+      keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-			opts.desc = "Show documentation for what is under cursor"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+      opts.desc = "Show documentation for what is under cursor"
+      keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-			opts.desc = "Restart LSP"
-			keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+      opts.desc = "Restart LSP"
+      keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 
-			-- Format using `:Format`
-			vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-				vim.lsp.buf.format({
-					bufnr = bufnr,
-					filter = function(client)
-						return client.name == "null-ls"
-					end,
-				})
-				print("File formatted")
-			end, { desc = "Format current buffer with LSP" })
-		end
+      -- Format using `:Format`
+      vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+        vim.lsp.buf.format({
+          bufnr = bufnr,
+          filter = function(client)
+            return client.name == "null-ls"
+          end,
+        })
+        print("File formatted")
+      end, { desc = "Format current buffer with LSP" })
+    end
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Change the Diagnostic symbols in the sign column (gutter) to be the same as the ones used by lualine
-		local signs = { Error = "󰅚 ", Warn = "󰀪 ", Info = "󰋽 ", Hint = "󰌶 " }
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-		end
+    -- Change the Diagnostic symbols in the sign column (gutter) to be the same as the ones used by lualine
+    local signs = { Error = "󰅚 ", Warn = "󰀪 ", Info = "󰋽 ", Hint = "󰌶 " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
 
-		-- Specify how the border looks like
-		local border = {
-			{ "╭", "FloatBorder" },
-			{ "─", "FloatBorder" },
-			{ "╮", "FloatBorder" },
-			{ "│", "FloatBorder" },
-			{ "╯", "FloatBorder" },
-			{ "─", "FloatBorder" },
-			{ "╰", "FloatBorder" },
-			{ "│", "FloatBorder" },
-		}
+    -- Specify how the border looks like
+    local border = "rounded"
+    -- {
+    --   { "╭", "FloatBorder" },
+    --   { "─", "FloatBorder" },
+    --   { "╮", "FloatBorder" },
+    --   { "│", "FloatBorder" },
+    --   { "╯", "FloatBorder" },
+    --   { "─", "FloatBorder" },
+    --   { "╰", "FloatBorder" },
+    --   { "│", "FloatBorder" },
+    -- }
 
-		-- Add the border on hover and on signature help
-		local handlers = {
-			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-		}
+    -- Add the border on hover and on signature help
+    local handlers = {
+      ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+      ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+    }
 
-		require("lspconfig.ui.windows").default_options = {
-			border = border,
-		}
+    require("lspconfig.ui.windows").default_options = {
+      border = border,
+    }
 
-		vim.diagnostic.config({
-			handlers = handlers,
-			source = false,
-			virtual_text = {
-				spacing = 2,
-				-- format = function(_) return "" end,
-				format = function(diagnostic)
-					-- Only show a short message for warnings and errors
-					if
-						diagnostic.severity == vim.diagnostic.severity.WARN
-						or diagnostic.severity == vim.diagnostic.severity.ERROR
-					then
-						local first_line = diagnostic.message:gmatch("[^\n]*")()
-						local first_sentence = string.match(first_line, "(.-%. )") or first_line
-						local first_lhs = string.match(first_sentence, "(.-): ") or first_sentence
-						return first_lhs
-					end
-					-- Don't show any message for info or hint
-					return ""
-				end,
-			},
-			float = {
-				header = "",
-				source = false,
-				border = border,
-				format = function(diagnostic)
-					-- Some diagnostics in F# are multiline but rendered as single line, this will make them easier to read.
-					return vim.fn.substitute(diagnostic.message, "\\s\\{2,}", "\n\n  ", "g")
-				end,
-			},
-		})
+    vim.diagnostic.config({
+      handlers = handlers,
+      source = false,
+      virtual_text = {
+        spacing = 2,
+        -- format = function(_) return "" end,
+        format = function(diagnostic)
+          -- Only show a short message for warnings and errors
+          if
+              diagnostic.severity == vim.diagnostic.severity.WARN
+              or diagnostic.severity == vim.diagnostic.severity.ERROR
+          then
+            local first_line = diagnostic.message:gmatch("[^\n]*")()
+            local first_sentence = string.match(first_line, "(.-%. )") or first_line
+            local first_lhs = string.match(first_sentence, "(.-): ") or first_sentence
+            return first_lhs
+          end
+          -- Don't show any message for info or hint
+          return ""
+        end,
+      },
+      float = {
+        -- header = "",
+        source = false,
+        border = border,
+        format = function(diagnostic)
+          -- Some diagnostics in F# are multiline but rendered as single line, this will make them easier to read.
+          return vim.fn.substitute(diagnostic.message, "\\s\\{2,}", "\n\n  ", "g")
+        end,
+      },
+    })
 
-		-- configure Ionide / FSharp server
-		require("ionide").setup({
-			handlers = handlers,
-			capabilities = capabilities,
-			on_attach = function(client, bufnr)
-				on_attach(client, bufnr)
-				vim.lsp.codelens.refresh()
-			end,
-		})
+    -- configure Ionide / FSharp server
+    require("ionide").setup({
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+        vim.lsp.codelens.refresh()
+      end,
+    })
 
-		-- configure elm server
-		lspconfig["elmls"].setup({
-			handlers = handlers,
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- configure elm server
+    lspconfig["elmls"].setup({
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure html server
-		lspconfig["html"].setup({
-			handlers = handlers,
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- configure html server
+    lspconfig["html"].setup({
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure typescript server with plugin
-		lspconfig["ts_ls"].setup({
-			handlers = handlers,
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- configure typescript server with plugin
+    lspconfig["ts_ls"].setup({
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure json server
-		lspconfig["jsonls"].setup({
-			handlers = handlers,
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- configure json server
+    lspconfig["jsonls"].setup({
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure gleam server
-		lspconfig["gleam"].setup({
-			handlers = handlers,
-			capabilities = capabilities,
-			on_attach = on_attach,
-		})
+    -- configure gleam server
+    lspconfig["gleam"].setup({
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
 
-		-- configure lua server (with special settings)
-		lspconfig["lua_ls"].setup({
-			handlers = handlers,
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = { -- custom settings for lua
-				Lua = {
-					-- make the language server recognize "vim" global
-					diagnostics = {
-						globals = { "vim" },
-					},
-					workspace = {
-						-- make language server aware of runtime files
-						library = {
-							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-							[vim.fn.stdpath("config") .. "/lua"] = true,
-						},
-						-- Use Neodev in config files
-						completion = {
-							callSnippet = "Replace",
-						},
-					},
-				},
-			},
-		})
-	end,
+    -- configure lua server (with special settings)
+    lspconfig["lua_ls"].setup({
+      handlers = handlers,
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = { -- custom settings for lua
+        Lua = {
+          -- make the language server recognize "vim" global
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            -- make language server aware of runtime files
+            library = {
+              [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+              [vim.fn.stdpath("config") .. "/lua"] = true,
+            },
+            -- Use Neodev in config files
+            completion = {
+              callSnippet = "Replace",
+            },
+          },
+        },
+      },
+    })
+  end,
 }
