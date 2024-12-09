7b99e9cd483d5ce4e333485f82e2b64c56fbd874
Anton Strand <anton.strand@insurello.se>
2024-12-09 13:53:06 +0100

    Change keybinding to source file

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,1 +1,1 @@
-vim.keymap.set("n", "<leader>ss", "<cmd>w<cr><cmd>source%<cr>", { desc = "Save and source file" })
+vim.keymap.set("n", "<space><space>x", "<cmd>w<cr><cmd>source%<cr>", { desc = "Save and source file" })
