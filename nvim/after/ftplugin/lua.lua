3a351466f05aa2e04303e1dc24b1c37f3d00f920
Anton Strand <anton.strand@insurello.se>
2024-12-09 13:53:32 +0100

    Add keybindings to run sections

--- nvim/after/ftplugin/lua.lua
+++ nvim/after/ftplugin/lua.lua
@@ -1,1 +1,3 @@
 vim.keymap.set("n", "<space><space>x", "<cmd>w<cr><cmd>source%<cr>", { desc = "Save and source file" })
+vim.keymap.set("n", "<leader>x", ":.lua<cr>", { desc = "Run current line" })
+vim.keymap.set("v", "<leader>x", ":lua<cr>", { desc = "Run current selection" })
