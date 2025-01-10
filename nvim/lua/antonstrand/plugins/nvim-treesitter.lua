7b53a38e1f20945491af9a2bd17f37f14d1c50f5
Anton Strand <anton.strand@insurello.se>
2025-01-10 10:28:10 +0100

    Disable treesitter for larger files

--- nvim/lua/antonstrand/plugins/nvim-treesitter.lua
+++ nvim/lua/antonstrand/plugins/nvim-treesitter.lua
@@ -15,6 +15,14 @@ return {
       treesitter.setup({ -- enable syntax highlighting
         highlight = {
           enable = true,
+          -- Don't turn on treesitter on larger files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
         },
         -- enable indentation
         indent = { enable = true },
@@ -56,7 +64,7 @@ return {
       })
 
       -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-      require('ts_context_commentstring').setup {}
+      require("ts_context_commentstring").setup({})
     end,
   },
 }
