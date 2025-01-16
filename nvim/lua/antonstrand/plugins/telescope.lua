cbf4b548aec38f88e26fbb700a4d304860db2b4d
Anton Strand <anton.strand@insurello.se>
2025-01-16 19:24:44 +0100

    Add shortcut to resume my last search

--- nvim/lua/antonstrand/plugins/telescope.lua
+++ nvim/lua/antonstrand/plugins/telescope.lua
@@ -52,9 +52,9 @@ return {
 
     telescope.load_extension("fzf")
 
-		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     -- set keymaps
     vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Resume fuzzy find" })
     vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
     vim.keymap.set(
       "n",
