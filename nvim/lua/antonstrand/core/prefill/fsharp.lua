37e4cfebd843b0a06e1d453ec6968579ce23592d
Anton Strand <anton.strand@insurello.se>
2025-02-09 19:53:35 +0100

    Fix prefill of F# files

--- nvim/lua/antonstrand/core/prefill/fsharp.lua
+++ nvim/lua/antonstrand/core/prefill/fsharp.lua
@@ -1,25 +1,24 @@
 local utils = require("antonstrand.core.prefill.helpers")
 
 local function is_domain_sweden()
-  local cur_path = vim.fn.expand("%:h")
-  return string.match(cur_path, "DomainSweden") ~= nil
+	local cur_path = vim.fn.expand("%:h")
+	return string.match(cur_path, "DomainSweden") ~= nil
 end
 
 local function create_template()
-  local module_name = utils.create_module_name()
-  if is_domain_sweden() then
-    return { "namespace " .. module_name, "", utils.filename() .. " =" }
-  else
-    return { "module " .. module_name }
-  end
+	local module_name = utils.create_module_name()
+	if is_domain_sweden() then
+		return { "namespace " .. module_name, "", "module " .. utils.filename() .. " =" }
+	else
+		return { "module " .. module_name }
+	end
 end
 
 local M = {}
 
-M.insert_template = function(bufnr)
-  local bufnr = bufnr or 0
-  local template = create_template()
-  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, template)
+M.insert_template = function()
+	local template = create_template()
+	vim.api.nvim_buf_set_lines(0, 0, -1, false, template)
 end
 
 return M
--- nvim/lua/antonstrand/core/prefill/init.lua
+++ nvim/lua/antonstrand/core/prefill/init.lua
@@ -1,13 +1,13 @@
 local prefill_group = vim.api.nvim_create_augroup("prefill_group", {})
 
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-  pattern = "*.elm",
-  group = prefill_group,
-  callback = require("antonstrand.core.prefill.elm").insert_module_name,
+	pattern = "*.elm",
+	group = prefill_group,
+	callback = require("antonstrand.core.prefill.elm").insert_module_name,
 })
 
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-  pattern = "*.fs",
-  group = prefill_group,
-  callback = require("antonstrand.core.prefill.fsharp").insert_template,
+	pattern = "*.fs",
+	group = prefill_group,
+	callback = require("antonstrand.core.prefill.fsharp").insert_template,
 })
