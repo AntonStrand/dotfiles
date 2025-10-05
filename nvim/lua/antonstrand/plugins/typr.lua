1e12790fa451d921d7138d748ba474d337ce4821
Anton Strand <anton.strand@insurello.se>
2025-10-05 14:48:59 +0200

    Add typr

--- nvim/lua/antonstrand/plugins/typr.lua
+++ nvim/lua/antonstrand/plugins/typr.lua
@@ -1,6 +1,20 @@
 return {
   "nvzone/typr",
-  cmd = "TyprStats",
   dependencies = "nvzone/volt",
-  opts = {},
+  opts = {
+    winlayout = "responsive",
+    kblayout = {
+      { "q", "w", "e", "r", "t", "y", "u", "i", "o", "p" },
+      { "a", "s", "d", "f", "g", "h", "j", "k", "l", ";" },
+      { "z", "x", "c", "v", "b", "n", "m", ",", ".", "/" },
+    },
+    wpm_goal = 120,
+    numbers = false,
+    symbols = false,
+    random = false,
+    insert_on_start = false,
+    stats_filepath = vim.fn.stdpath("cache") .. "/typrstats",
+    mappings = nil,
+  },
+  cmd = { "Typr", "TyprStats" },
 }
