88e26851706a950e8d0ef8e7b732c6d263cb0339
Anton Strand <anton.strand@insurello.se>
2025-10-05 13:30:18 +0200

    Format

--- nvim/after/ftplugin/fsharp.lua
+++ nvim/after/ftplugin/fsharp.lua
@@ -1,120 +1,120 @@
 local function find_parent(types, node)
-	if not node then
-		return nil
-	end
+  if not node then
+    return nil
+  end
 
-	if vim.tbl_contains(types, node:type()) then
-		return node
-	end
+  if vim.tbl_contains(types, node:type()) then
+    return node
+  end
 
-	local next_node = node:parent()
+  local next_node = node:parent()
 
-	if not next_node then
-		next_node = node:next_sibling()
-		print("try sibling" .. vim.inspect(next_node))
-		return nil
-	end
+  if not next_node then
+    next_node = node:next_sibling()
+    print("try sibling" .. vim.inspect(next_node))
+    return nil
+  end
 
-	return find_parent(types, next_node)
+  return find_parent(types, next_node)
 end
 
 local function get_root(bufnr)
-	local parser = vim.treesitter.get_parser(bufnr, "fsharp", {})
-	local tree = parser:parse()[1]
-	return tree:root()
+  local parser = vim.treesitter.get_parser(bufnr, "fsharp", {})
+  local tree = parser:parse()[1]
+  return tree:root()
 end
 
 local function move_cursor(closest_match)
-	if not closest_match then
-		return
-	end
+  if not closest_match then
+    return
+  end
 
-	local _, col = unpack(vim.api.nvim_win_get_cursor(0))
+  local _, col = unpack(vim.api.nvim_win_get_cursor(0))
 
-	local cursor_row, cursor_col = closest_match:start()
+  local cursor_row, cursor_col = closest_match:start()
 
-	if closest_match:type() == "function_declaration_left" then
-		local line_width = #vim.api.nvim_get_current_line()
+  if closest_match:type() == "function_declaration_left" then
+    local line_width = #vim.api.nvim_get_current_line()
 
-		if line_width > col then
-			cursor_col = col
-		else
-			cursor_col = 0
-		end
-	end
+    if line_width > col then
+      cursor_col = col
+    else
+      cursor_col = 0
+    end
+  end
 
-	-- The row is a line above the actual function definition
-	vim.api.nvim_win_set_cursor(0, { cursor_row + 1, cursor_col })
+  -- The row is a line above the actual function definition
+  vim.api.nvim_win_set_cursor(0, { cursor_row + 1, cursor_col })
 end
 
 local function move_up(query)
-	local function_definitions = vim.treesitter.query.parse("fsharp", query)
+  local function_definitions = vim.treesitter.query.parse("fsharp", query)
 
-	local bufnr = vim.api.nvim_get_current_buf()
-	local root = get_root(bufnr)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local root = get_root(bufnr)
 
-	local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
-	local closest_function = nil
+  local closest_function = nil
 
-	-- By starting at the row above the cursor it will make sure that the
-	-- last capture is the closest function above.
-	for _, fn in function_definitions:iter_captures(root, bufnr, 0, row - 1) do
-		closest_function = fn
-	end
+  -- By starting at the row above the cursor it will make sure that the
+  -- last capture is the closest function above.
+  for _, fn in function_definitions:iter_captures(root, bufnr, 0, row - 1) do
+    closest_function = fn
+  end
 
-	move_cursor(closest_function)
+  move_cursor(closest_function)
 end
 
 local function move_down(query)
-	local function_definitions = vim.treesitter.query.parse("fsharp", query)
+  local function_definitions = vim.treesitter.query.parse("fsharp", query)
 
-	local bufnr = vim.api.nvim_get_current_buf()
-	local root = get_root(bufnr)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local root = get_root(bufnr)
 
-	local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
 
-	local closest_function = nil
+  local closest_function = nil
 
-	-- By starting at the row above the cursor it will make sure that the
-	-- last capture is the closest function above.
-	for _, fn in function_definitions:iter_captures(root, bufnr, row + 1, -1) do
-		local s_row, _, e_row, _ = fn:range()
+  -- By starting at the row above the cursor it will make sure that the
+  -- last capture is the closest function above.
+  for _, fn in function_definitions:iter_captures(root, bufnr, row + 1, -1) do
+    local s_row, _, e_row, _ = fn:range()
 
-		if not closest_function and s_row > row then
-			closest_function = fn
-		end
-	end
+    if not closest_function and s_row > row then
+      closest_function = fn
+    end
+  end
 
-	move_cursor(closest_function)
+  move_cursor(closest_function)
 end
 
 local function goto_parent_function()
-	move_up("(function_declaration_left) @function (function_expression) @function (fun_expression) @function")
+  move_up("(function_declaration_left) @function (function_expression) @function (fun_expression) @function")
 end
 
 local function goto_next_top_function()
-	move_up("(function_declaration_left) @function")
+  move_up("(function_declaration_left) @function")
 end
 
 local function goto_next_function()
-	move_down("(function_declaration_left) @function (function_expression) @function (fun_expression) @function")
+  move_down("(function_declaration_left) @function (function_expression) @function (fun_expression) @function")
 end
 
 local function goto_next_bottom_function()
-	move_down("(function_declaration_left) @function")
+  move_down("(function_declaration_left) @function")
 end
 
 local function goto_prev_match()
-	move_up(
-		"(match_expression (rules (rule (identifier_pattern (long_identifier_or_op) @match) ))) (match_expression (rules (rule (const) @match)))"
-	)
+  move_up(
+    "(match_expression (rules (rule (identifier_pattern (long_identifier_or_op) @match) ))) (match_expression (rules (rule (const) @match)))"
+  )
 end
 
 local function goto_next_match()
-	move_down(
-		"(match_expression (rules (rule (identifier_pattern (long_identifier_or_op) @match) ))) (match_expression (rules (rule (const) @match)))"
-	)
+  move_down(
+    "(match_expression (rules (rule (identifier_pattern (long_identifier_or_op) @match) ))) (match_expression (rules (rule (const) @match)))"
+  )
 end
 
 -- local function outer_function()
@@ -122,21 +122,21 @@ end
 -- end
 
 local function outer_function()
-	local current_node = vim.treesitter.get_node()
-	local parent = find_parent({ "declaration_expression" }, current_node)
-	if not parent then
-		print("No parent found")
-		return
-	end
-	local srow, scol, erow, ecol = parent:range()
+  local current_node = vim.treesitter.get_node()
+  local parent = find_parent({ "declaration_expression" }, current_node)
+  if not parent then
+    print("No parent found")
+    return
+  end
+  local srow, scol, erow, ecol = parent:range()
 
-	local row_span = erow - srow
+  local row_span = erow - srow
 
-	vim.cmd([[ execute "normal! \<ESC>" ]]) -- Delete if there is anything in the command line already
-	vim.cmd(tostring(srow + 1)) -- go to the first row of the function
-	vim.cmd("normal! V" .. row_span .. "j") -- visual select from first row to the last
+  vim.cmd([[ execute "normal! \<ESC>" ]]) -- Delete if there is anything in the command line already
+  vim.cmd(tostring(srow + 1))            -- go to the first row of the function
+  vim.cmd("normal! V" .. row_span .. "j") -- visual select from first row to the last
 
-	print(vim.inspect(parent:named()), vim.inspect(srow), vim.inspect(scol), vim.inspect(erow), vim.inspect(ecol))
+  print(vim.inspect(parent:named()), vim.inspect(srow), vim.inspect(scol), vim.inspect(erow), vim.inspect(ecol))
 end
 
 vim.keymap.set("n", "[f", goto_parent_function, { silent = true, noremap = true })
--- nvim/after/ftplugin/oil.lua
+++ nvim/after/ftplugin/oil.lua
@@ -1,171 +1,111 @@
 local oil = require("oil")
 
 local function copy(path)
-  local cmd = string.format([[osascript -e 'set the clipboard to POSIX file "%s"' ]], path)
-  local result = vim.fn.system(cmd)
-  if vim.v.shell_error ~= 0 then
-    vim.notify("Copy failed: " .. result, vim.log.levels.ERROR)
-  else
-    vim.notify("Copied to system clipboard", vim.log.levels.INFO)
-  end
+	local cmd = string.format([[osascript -e 'set the clipboard to POSIX file "%s"' ]], path)
+	local result = vim.fn.system(cmd)
+	if vim.v.shell_error ~= 0 then
+		vim.notify("Copy failed: " .. result, vim.log.levels.ERROR)
+	else
+		vim.notify("Copied to system clipboard", vim.log.levels.INFO)
+	end
 end
 
 local function yank_entry(entry)
-  local path = oil.get_current_dir(0)
+	local path = oil.get_current_dir(0)
 
-  if path and entry then
-    -- Escape the path for shell command
-    copy(vim.fn.fnameescape(path .. entry.parsed_name))
-  else
-    vim.notify("No file or directory selected", vim.log.levels.WARN)
-  end
+	if path and entry then
+		-- Escape the path for shell command
+		copy(vim.fn.fnameescape(path .. entry.parsed_name))
+	else
+		vim.notify("No file or directory selected", vim.log.levels.WARN)
+	end
 end
 
 local function zip_and_yank()
-  local path = oil.get_current_dir(0)
-  local entry = oil.get_cursor_entry()
-  if path and entry then
-    local name = entry.name
-    local parent_dir = vim.fn.fnamemodify(path, ":h")               -- Get the parent directory
-    local timestamp = os.date("%y%m%d%H%M%S")                       -- Append timestamp to avoid duplicates
-    local zip_path = string.format("/tmp/%s_%s.zip", name, timestamp) -- Path in macOS's tmp directory
-    -- Create the zip file
-    local zip_cmd = string.format(
-      "cd %s && zip -r %s %s",
-      vim.fn.shellescape(parent_dir),
-      vim.fn.shellescape(zip_path),
-      vim.fn.shellescape(name)
-    )
-    local result = vim.fn.system(zip_cmd)
-    if vim.v.shell_error ~= 0 then
-      vim.notify("Failed to create zip file: " .. result, vim.log.levels.ERROR)
-      return
-    end
-    -- Copy the zip file to the system clipboard
-    copy(zip_path)
-  else
-    vim.notify("No file or directory selected", vim.log.levels.WARN)
-  end
-end
-
-local function visual_selection_range()
-  -- If we don't change to normal mode the previous selection will be used.
-  vim.cmd([[ execute "normal! \<ESC>" ]])
-  local _, start_row, _, _ = unpack(vim.fn.getpos("'<"))
-  local _, end_row, _, _ = unpack(vim.fn.getpos("'>"))
-  local timestamp = os.date("%y%m%d%H%M%S") -- Append timestamp to avoid duplicates
-  local tmp_dir_name = string.format("copy_%s", timestamp)
-  local tmp_dir = string.format("/tmp/%s", tmp_dir_name)
-  vim.fn.system(string.format("mkdir -p %s", tmp_dir))
-
-  local path = oil.get_current_dir(0)
-  for row = start_row, end_row do
-    local entry = oil.get_entry_on_line(0, row)
-
-    if entry then
-      local escaped_path = vim.fn.fnameescape(path .. entry.parsed_name)
-      local tmp_file = vim.fn.fnameescape(tmp_dir .. "/" .. entry.parsed_name .. "/")
-      local cmd = string.format("cp %s %s", escaped_path, tmp_dir)
-      vim.fn.system(cmd)
-    end
-  end
-
-  local zip_cmd =
-      string.format("cd /tmp && zip -r %s.zip %s", vim.fn.shellescape(tmp_dir), vim.fn.shellescape(tmp_dir_name))
-
-  local result = vim.fn.system(zip_cmd)
-
-  if vim.v.shell_error ~= 0 then
-    vim.notify("Failed to create zip file: " .. result, vim.log.levels.ERROR)
-    return
-  end
-
-  local copy_cmd =
-      string.format([[osascript -e 'set the clipboard to POSIX file "%s"' ]], vim.fn.fnameescape(tmp_dir))
-
-  local copy_result = vim.fn.system(copy_cmd)
-  if vim.v.shell_error ~= 0 then
-    vim.notify("Failed to copy files to clipboard: " .. copy_result, vim.log.levels.ERROR)
-    return
-  end
-  vim.notify("Files copied to clipboard", vim.log.levels.INFO)
+	local path = oil.get_current_dir(0)
+	local entry = oil.get_cursor_entry()
+	if path and entry then
+		local name = entry.name
+		local parent_dir = vim.fn.fnamemodify(path, ":h") -- Get the parent directory
+		local timestamp = os.date("%y%m%d%H%M%S") -- Append timestamp to avoid duplicates
+		local zip_path = string.format("/tmp/%s_%s.zip", name, timestamp) -- Path in macOS's tmp directory
+		-- Create the zip file
+		local zip_cmd = string.format(
+			"cd %s && zip -r %s %s",
+			vim.fn.shellescape(parent_dir),
+			vim.fn.shellescape(zip_path),
+			vim.fn.shellescape(name)
+		)
+		local result = vim.fn.system(zip_cmd)
+		if vim.v.shell_error ~= 0 then
+			vim.notify("Failed to create zip file: " .. result, vim.log.levels.ERROR)
+			return
+		end
+		-- Copy the zip file to the system clipboard
+		copy(zip_path)
+	else
+		vim.notify("No file or directory selected", vim.log.levels.WARN)
+	end
 end
 
 local function selected_row_range()
-  vim.cmd([[ execute "normal! \<ESC>" ]])
-  local _, start_row, _, _ = unpack(vim.fn.getpos("'<"))
-  local _, end_row, _, _ = unpack(vim.fn.getpos("'>"))
+	vim.cmd([[ execute "normal! \<ESC>" ]])
+	local _, start_row, _, _ = unpack(vim.fn.getpos("'<"))
+	local _, end_row, _, _ = unpack(vim.fn.getpos("'>"))
 
-  return start_row, end_row
+	return start_row, end_row
 end
 
 local function group_entries(start_row, end_row)
-  local timestamp = os.date("%y%m%d%H%M%S") -- Append timestamp to avoid duplicates
-  local tmp_dir_name = string.format("copy_%s", timestamp)
-  local tmp_dir = string.format("/tmp/%s", tmp_dir_name)
-  vim.fn.system(string.format("mkdir -p %s", tmp_dir))
+	local timestamp = os.date("%y%m%d%H%M%S") -- Append timestamp to avoid duplicates
+	local tmp_dir_name = string.format("copy_%s", timestamp)
+	local tmp_dir = string.format("/tmp/%s", tmp_dir_name)
+	vim.fn.system(string.format("mkdir -p %s", tmp_dir))
 
-  local path = oil.get_current_dir(0)
+	local path = oil.get_current_dir(0)
 
-  for row = start_row, end_row do
-    local entry = oil.get_entry_on_line(0, row)
+	for row = start_row, end_row do
+		local entry = oil.get_entry_on_line(0, row)
 
-    if entry then
-      local escaped_path = vim.fn.fnameescape(path .. entry.parsed_name)
-      local cmd = string.format("cp -R %s %s", escaped_path, tmp_dir)
-      vim.fn.system(cmd)
-    end
-  end
-  return tmp_dir
-  -- local zip = string.format("%s.zip", tmp_dir)
-  -- local zip_cmd = string.format("cd /tmp && zip -r %s %s", vim.fn.shellescape(zip), vim.fn.shellescape(tmp_dir_name))
-  -- local result = vim.fn.system(zip_cmd)
-  -- if vim.v.shell_error ~= 0 then
-  --   vim.notify("Failed to create zip file: " .. result, vim.log.levels.ERROR)
-  --   return
-  -- end
-  -- local copy_cmd =
-  --     string.format([[osascript -e 'set the clipboard to POSIX file "%s"' ]], vim.fn.fnameescape(tmp_dir))
-  --
-  -- local copy_result = vim.fn.system(copy_cmd)
-  -- if vim.v.shell_error ~= 0 then
-  --   vim.notify("Failed to copy files to clipboard: " .. copy_result, vim.log.levels.ERROR)
-  --   return
-  -- end
-  -- vim.notify("Files copied to clipboard", vim.log.levels.INFO)
+		if entry then
+			local escaped_path = vim.fn.fnameescape(path .. entry.parsed_name)
+			local cmd = string.format("cp -R %s %s", escaped_path, tmp_dir)
+			vim.fn.system(cmd)
+		end
+	end
+	return tmp_dir
 end
 
 local function yank_current()
-  yank_entry(oil.get_cursor_entry())
+	yank_entry(oil.get_cursor_entry())
 end
 
 local function yank_selection()
-  local start_row, end_row = selected_row_range()
-  if start_row == end_row then
-    yank_entry(oil.get_entry_on_line(0, start_row))
-    return
-  end
-  copy(group_entries(start_row, end_row))
+	local start_row, end_row = selected_row_range()
+	if start_row == end_row then
+		yank_entry(oil.get_entry_on_line(0, start_row))
+		return
+	end
+	copy(group_entries(start_row, end_row))
 end
 
 local function zip_selection()
-  local start_row, end_row = selected_row_range()
-  if start_row == end_row then
-    yank_entry(oil.get_entry_on_line(0, start_row))
-    return
-  end
-  local tmp_dir = group_entries(start_row, end_row)
-  local dir = vim.fn.fnamemodify(tmp_dir, ":t:S")
-  print(dir)
-  local zip_path = vim.fn.shellescape(string.format("%s.zip", tmp_dir))
-  local zip_cmd = string.format("cd /tmp && zip -r %s %s", zip_path, dir)
+	local start_row, end_row = selected_row_range()
+	if start_row == end_row then
+		yank_entry(oil.get_entry_on_line(0, start_row))
+		return
+	end
+	local tmp_dir = group_entries(start_row, end_row)
+	local dir = vim.fn.fnamemodify(tmp_dir, ":t:S")
+	local zip_path = vim.fn.shellescape(string.format("%s.zip", tmp_dir))
+	local zip_cmd = string.format("cd /tmp && zip -r %s %s", zip_path, dir)
 
-  local result = vim.fn.system(zip_cmd)
-  if vim.v.shell_error ~= 0 then
-    vim.notify("Failed to create zip file: " .. result, vim.log.levels.ERROR)
-    return
-  end
-  copy(zip_path)
+	local result = vim.fn.system(zip_cmd)
+	if vim.v.shell_error ~= 0 then
+		vim.notify("Failed to create zip file: " .. result, vim.log.levels.ERROR)
+		return
+	end
+	copy(zip_path)
 end
 
 local opts = { noremap = true, silent = true, buffer = 0 }
