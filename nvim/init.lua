ae694809f416d44855df92da0836a894cdc67219
Anton Strand <anton.strand@insurello.se>
2026-02-08 23:02:57 +0100

    Fix depricated lsp settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,3 @@
 require("antonstrand.core")
 require("antonstrand.lazy")
+require("antonstrand.lsp")
--- nvim/lua/antonstrand/plugins/lsp/mason.lua
+++ nvim/lua/antonstrand/plugins/lsp/mason.lua
@@ -1,30 +1,7 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
+	{
 		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
-
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-
-		local mason_tool_installer = require("mason-tool-installer")
-
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
-			},
-		})
-
-		mason_lspconfig.setup({
+		opts = {
 			-- list of servers for mason to install
 			ensure_installed = {
 				"elmls",
@@ -36,22 +13,36 @@ return {
 				"graphql",
 				"emmet_ls",
 				"prismals",
-				"pyright",
 				"jsonls",
 			},
-			-- auto-install configured servers (with lspconfig)
-			automatic_installation = true, -- not the same as ensure_installed
-		})
-
-		mason_tool_installer.setup({
+		},
+		dependencies = {
+			{
+				"williamboman/mason.nvim",
+				opts = {
+					ui = {
+						icons = {
+							package_installed = "✓",
+							package_pending = "➜",
+							package_uninstalled = "✗",
+						},
+					},
+				},
+			},
+			"neovim/nvim-lspconfig",
+		},
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = {
 			ensure_installed = {
 				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint", -- python linter
 				"eslint_d", -- js linter
 			},
-		})
-	end,
+		},
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+	},
 }
