225abdfde163ca4758afa3b5e30bafb0ac7be42c
Anton Strand <anton.strand@savr.se>
2026-02-15 13:48:05 +0100

    No more zen mode

--- nvim/lua/antonstrand/plugins/zen-mode.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "folke/zen-mode.nvim",
-  config = function()
-    local zen_mode = require("zen-mode")
-    vim.keymap.set("n", "<leader>z", zen_mode.toggle, { noremap = true, silent = true, desc = "Toggle Zen Mode" })
-  end,
-}
