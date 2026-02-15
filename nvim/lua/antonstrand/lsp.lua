db14c9e0f6947952cba0d6ec6af0decaabb884e5
Anton Strand <anton.strand@savr.se>
2026-02-15 21:05:04 +0100

    Format

--- nvim/lua/antonstrand/lsp.lua
+++ nvim/lua/antonstrand/lsp.lua
@@ -5,81 +5,81 @@ vim.lsp.enable("html")
 
 local keymap = vim.keymap -- for conciseness
 vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf, silent = true }
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf, silent = true }
 
-		-- set keybinds
-		opts.desc = "Show LSP references"
-		keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+    -- set keybinds
+    opts.desc = "Show LSP references"
+    keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
 
-		opts.desc = "Go to declaration"
-		keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    opts.desc = "Go to declaration"
+    keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
 
-		opts.desc = "Show LSP definition"
-		keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    opts.desc = "Show LSP definition"
+    keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 
-		opts.desc = "Show LSP implementations"
-		keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+    opts.desc = "Show LSP implementations"
+    keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
 
-		opts.desc = "Show LSP type definitions"
-		keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+    opts.desc = "Show LSP type definitions"
+    keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-		opts.desc = "See available code actions"
-		keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+    opts.desc = "See available code actions"
+    keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 
-		opts.desc = "Smart rename"
-		keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    opts.desc = "Smart rename"
+    keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 
-		opts.desc = "Show buffer diagnostics"
-		keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+    opts.desc = "Show buffer diagnostics"
+    keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-		opts.desc = "Show line diagnostics"
-		keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+    opts.desc = "Show line diagnostics"
+    keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-		opts.desc = "Go to previous diagnostic"
-		keymap.set("n", "[d", function()
-			vim.diagnostic.jump({ count = -1, float = true })
-		end, opts)
-		--
-		opts.desc = "Go to next diagnostic"
-		keymap.set("n", "]d", function()
-			vim.diagnostic.jump({ count = 1, float = true })
-		end, opts)
+    opts.desc = "Go to previous diagnostic"
+    keymap.set("n", "[d", function()
+      vim.diagnostic.jump({ count = -1, float = true })
+    end, opts)
+    --
+    opts.desc = "Go to next diagnostic"
+    keymap.set("n", "]d", function()
+      vim.diagnostic.jump({ count = 1, float = true })
+    end, opts)
 
-		opts.desc = "Show documentation for what is under cursor"
-		keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    opts.desc = "Show documentation for what is under cursor"
+    keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-		opts.desc = "Restart LSP"
-		keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
-	end,
+    opts.desc = "Restart LSP"
+    keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+  end,
 })
 
 local severity = vim.diagnostic.severity
 vim.diagnostic.config({
-	signs = {
-		text = {
-			[severity.ERROR] = "󰅚 ",
-			[severity.WARN] = "󰀪 ",
-			[severity.HINT] = "󰌶 ",
-			[severity.INFO] = "󰋽 ",
-		},
-	},
-	virtual_text = {
-		spacing = 2,
-		format = function(diagnostic)
-			-- Don't show any message for info or hint
-			if diagnostic.severity == severity.INFO or diagnostic.severity == severity.HINT then
-				return ""
-			end
+  signs = {
+    text = {
+      [severity.ERROR] = "󰅚 ",
+      [severity.WARN] = "󰀪 ",
+      [severity.HINT] = "󰌶 ",
+      [severity.INFO] = "󰋽 ",
+    },
+  },
+  virtual_text = {
+    spacing = 2,
+    format = function(diagnostic)
+      -- Don't show any message for info or hint
+      if diagnostic.severity == severity.INFO or diagnostic.severity == severity.HINT then
+        return ""
+      end
 
-			-- Only show a short message for warnings and errors
-			local first_line = diagnostic.message:gmatch("[^\n]*")()
-			local first_sentence = string.match(first_line, "(.-%. )") or first_line
-			local first_lhs = string.match(first_sentence, "(.-): ") or first_sentence
-			return first_lhs
-		end,
-	},
+      -- Only show a short message for warnings and errors
+      local first_line = diagnostic.message:gmatch("[^\n]*")()
+      local first_sentence = string.match(first_line, "(.-%. )") or first_line
+      local first_lhs = string.match(first_sentence, "(.-): ") or first_sentence
+      return first_lhs
+    end,
+  },
 })
