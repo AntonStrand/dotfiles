21a8316579633286a4853205211aab4b6e6c12bf
Anton Strand <anton.strand@savr.se>
2026-02-15 10:08:45 +0100

    Use <C-p> and <C-n> to navigate

--- nvim/lua/antonstrand/plugins/outline.lua
+++ nvim/lua/antonstrand/plugins/outline.lua
@@ -1,12 +1,15 @@
 return {
-	"hedyhli/outline.nvim",
-	lazy = true,
-	cmd = { "Outline" },
-	keys = {
-	},
-	opts = {},
+  "hedyhli/outline.nvim",
+  lazy = true,
+  cmd = { "Outline" },
+  keys = {
     { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+  },
   opts = {
+    keymaps = {
+      up_and_jump = "<C-p>",
+      down_and_jump = "<C-n>",
+    },
     outline_items = {
       show_symbol_lineno = true,
     },
