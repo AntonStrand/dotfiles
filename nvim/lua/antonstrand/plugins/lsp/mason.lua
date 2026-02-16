fac95b38a9e3a7a5fd549053ca656735e94697f6
Anton Strand <anton.strand@savr.se>
2026-02-16 08:47:02 +0100

    Format

--- nvim/lua/antonstrand/plugins/lsp/mason.lua
+++ nvim/lua/antonstrand/plugins/lsp/mason.lua
@@ -1,48 +1,45 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			-- list of servers for mason to install
-			ensure_installed = {
-				"elmls",
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"jsonls",
-			},
-		},
-		dependencies = {
-			{
-				"williamboman/mason.nvim",
-				opts = {
-					ui = {
-						icons = {
-							package_installed = "✓",
-							package_pending = "➜",
-							package_uninstalled = "✗",
-						},
-					},
-				},
-			},
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		opts = {
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"eslint_d", -- js linter
-			},
-		},
-		dependencies = {
-			"williamboman/mason.nvim",
-		},
-	},
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      -- list of servers for mason to install
+      ensure_installed = {
+        "elmls",
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "lua_ls",
+        "jsonls",
+      },
+    },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
+          },
+        },
+      },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "eslint_d", -- js linter
+      },
+    },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+  },
 }
