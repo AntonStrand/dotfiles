253af4a8e0cb929408771335a1fd994ff21f32f3
Anton Strand <anton.strand@savr.com>
2026-02-21 11:51:28 +0100

    Update yank_highlight to not use depricated function

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,9 +1,13 @@
+local function augroup(name)
+	return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
+end
+
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight selection on yank",
-	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	group = augroup("highlight_yank"),
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank({ visual = true, timeout = 200 })
+		(vim.hl or vim.highlight).on_yank({ visual = true, timeout = 200 })
 	end,
 })
