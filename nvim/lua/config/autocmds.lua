455d60f880246855f9867a7f22669254d57f4799
Anton Strand <anton.strand@savr.com>
2026-02-21 11:52:01 +0100

    Open buffer at last loc

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -11,3 +11,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		(vim.hl or vim.highlight).on_yank({ visual = true, timeout = 200 })
 	end,
 })
+
+-- Go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+	group = augroup("last_loc"),
+	callback = function(event)
+		local exclude = { "gitcommit" }
+		local buf = event.buf
+		if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+			return
+		end
+		vim.b[buf].lazyvim_last_loc = true
+		local mark = vim.api.nvim_buf_get_mark(buf, '"')
+		local lcount = vim.api.nvim_buf_line_count(buf)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
