aa45d3dff9d7e64066948307016e59c19b3e0439
Anton Strand <anton.strand@savr.com>
2026-02-22 08:17:58 +0100

    Configure root_dir instead of using root_markers

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -18,7 +18,22 @@ vim.lsp.config("tailwindcss", {
 		"javascript",
 		"typescript",
 	},
-	root_markers = { ".git" },
+	root_dir = function(bufnr, on_dir)
+		local util = require("lspconfig.util")
+		local root_files = {
+			-- Generic
+			"tailwind.config.js",
+			"tailwind.config.cjs",
+			"tailwind.config.mjs",
+			"tailwind.config.ts",
+			-- Fallback for tailwind v4, where tailwind.config.* is not required anymore
+			".git",
+		}
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		root_files = util.insert_package_json(root_files, "tailwindcss", fname)
+		root_files = util.root_markers_with_field(root_files, { "mix.lock", "Gemfile.lock" }, "tailwind", fname)
+		on_dir(vim.fs.dirname(vim.fs.find(root_files, { path = fname, upward = true })[1]))
+	end,
 	capabilities = {
 		workspace = {
 			didChangeWatchedFiles = {
