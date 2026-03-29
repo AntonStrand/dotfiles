1a04dccc6a8ddd4de7d8ccc1b3891e43b88e3894
Anton Strand <anton.strand@savr.com>
2026-03-29 11:39:54 +0200

    Change formatter plugin

---
+++ nvim/lua/plugins/formatter.lua
@@ -0,0 +1,42 @@
+local priotizedFormatter = { "biome", "prettier", stop_after_first = true }
+
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	keys = {
+		{
+			"<leader>cf",
+			function()
+				require("conform").format({ async = true }, function(err, did_edit)
+					if not err and did_edit then
+						vim.notify("Code formatted", vim.log.levels.INFO, { title = "Conform" })
+					elseif err then
+						vim.notify("Code formatting failed", vim.log.levels.ERROR, { title = "Conform" })
+					end
+				end)
+			end,
+			mode = "",
+			desc = "[F]ormat buffer async",
+		},
+	},
+	opts = {
+		notify_on_error = true,
+		formatters_by_ft = {
+			lua = { "stylua" },
+			elm = { "elm_format" },
+			markdown = { "prettier" },
+			html = priotizedFormatter,
+			json = priotizedFormatter,
+			yaml = priotizedFormatter,
+			css = priotizedFormatter,
+			javascript = priotizedFormatter,
+			typescript = priotizedFormatter,
+			javascriptreact = priotizedFormatter,
+			typescriptreact = priotizedFormatter,
+		},
+		format_on_save = {
+			timeout_ms = 1000,
+			lsp_format = "fallback",
+		},
+	},
+}
