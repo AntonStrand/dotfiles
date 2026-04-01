34b310af177ad03f355efaf83aff75f94a77a776
Anton Strand <anton.strand@savr.com>
2026-04-01 22:33:51 +0200

    Format file

--- bin/brew.sh
+++ bin/brew.sh
@@ -7,72 +7,72 @@ echo "Checking if Homebrew is already installed..."
 
 # Checks if Homebrew is installed
 if test ! $(which brew); then
-	echo "Installing Homebrew..."
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-	(
-		echo
-		echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
-	) >>~/.zprofile
-	eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  (
+    echo
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'
+  ) >>~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
-	echo "Homebrew is already installed..."
+  echo "Homebrew is already installed..."
 fi
 
 echo "Installing packages..."
 apps=(
-	awscli
-	bat
-	cask
-	cfn-lint
-	cmake
-	elm
-	elm-format
-	fd
-	fzf
-	git
-	git-delta
-	gh
-	jq
-	koekeishiya/formulae/skhd
-	lazygit
-	neovim
-	node
-	nvm
-	postgresql
-	python
-	ripgrep
-	shellcheck
-	shfmt
-	starship
-	tmux
-	tree
-	tree-sitter
-	wget
-	yabai
-	zoxide
-	lua-language-server
-	font-hack-nerd-font
+  awscli
+  bat
+  cask
+  cfn-lint
+  cmake
+  elm
+  elm-format
+  fd
+  fzf
+  git
+  git-delta
+  gh
+  jq
+  koekeishiya/formulae/skhd
+  lazygit
+  neovim
+  node
+  nvm
+  postgresql
+  python
+  ripgrep
+  shellcheck
+  shfmt
+  starship
+  tmux
+  tree
+  tree-sitter
+  wget
+  yabai
+  zoxide
+  lua-language-server
+  font-hack-nerd-font
 )
 
 installed_apps=$(brew list)
 
 for app in ${apps[@]}; do
-	(echo "${installed_apps[@]}" | fgrep -q "$app") && echo "$app is already installed" || brew install "${app}"
+  (echo "${installed_apps[@]}" | fgrep -q "$app") && echo "$app is already installed" || brew install "${app}"
 done
 
 casks=(
-	bankid
-	docker
-	dotnet-sdk
-	spotify
-	ghostty
+  bankid
+  docker
+  dotnet-sdk
+  spotify
+  ghostty
 )
 
 echo "Installing casks..."
 installed_casks=$(brew list --cask)
 
 for cask in ${casks[@]}; do
-	(echo "${installed_casks[@]}" | fgrep -q "$cask") && echo "$cask is already installed" || brew install --cask "${cask}"
+  (echo "${installed_casks[@]}" | fgrep -q "$cask") && echo "$cask is already installed" || brew install --cask "${cask}"
 done
 
 # Update and Upgrade
