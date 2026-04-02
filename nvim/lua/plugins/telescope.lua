149d71c36d679cfb40033dc1fcc26d8eeb2b5a86
Anton Strand <anton.strand@savr.com>
2026-04-02 21:50:01 +0200

    Upgrade version

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,100 +11,100 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 local function filenameFirst(_, path)
-  local tail = vim.fs.basename(path)
-  local parent = vim.fn.fnamemodify(vim.fs.dirname(path), ":p:~:.")
-  if parent == "." then
-    return tail
-  end
-  return string.format("%s%s%s", tail, delimiter, parent)
+	local tail = vim.fs.basename(path)
+	local parent = vim.fn.fnamemodify(vim.fs.dirname(path), ":p:~:.")
+	if parent == "." then
+		return tail
+	end
+	return string.format("%s%s%s", tail, delimiter, parent)
 end
 
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-telescope/telescope-live-grep-args.nvim",
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
-    local lga_actions = require("telescope-live-grep-args.actions")
+	"nvim-telescope/telescope.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-telescope/telescope-live-grep-args.nvim",
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local actions = require("telescope.actions")
+		local lga_actions = require("telescope-live-grep-args.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = filenameFirst,
-        mappings = {
-          i = {                                    -- Insert mode
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-p>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-n>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-          },
-          n = {
-            ["dd"] = actions.delete_buffer,
-            ["q"] = actions.close,
-          },
-        },
-      },
-      extensions = {
-        live_grep_args = {
-          auto_quoting = true, -- enable/disable auto-quoting
-          mappings = {    -- extend mappings
-            i = {
-              ["<C-k>"] = lga_actions.quote_prompt(),
-              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
-              ["<C-f>"] = lga_actions.quote_prompt({ postfix = " -g " }),
-              ["<C-t>"] = lga_actions.quote_prompt({ postfix = " -t " }),
-              -- freeze the current list and start a fuzzy search in the frozen list
-              ["<C-space>"] = actions.to_fuzzy_refine,
-            },
-          },
-          -- ... also accepts theme settings, for example:
-          -- theme = "dropdown", -- use dropdown theme
-          -- theme = { }, -- use own theme spec
-          -- layout_config = { mirror=true }, -- mirror preview pane
-        },
-      },
-    })
+		telescope.setup({
+			defaults = {
 				preview = { treesitter = false },
+				path_display = filenameFirst,
+				mappings = {
+					i = { -- Insert mode
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-p>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+						["<C-n>"] = actions.move_selection_next, -- move to next result
+						["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+					},
+					n = {
+						["dd"] = actions.delete_buffer,
+						["q"] = actions.close,
+					},
+				},
+			},
+			extensions = {
+				live_grep_args = {
+					auto_quoting = true, -- enable/disable auto-quoting
+					mappings = { -- extend mappings
+						i = {
+							["<C-k>"] = lga_actions.quote_prompt(),
+							["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+							["<C-f>"] = lga_actions.quote_prompt({ postfix = " -g " }),
+							["<C-t>"] = lga_actions.quote_prompt({ postfix = " -t " }),
+							-- freeze the current list and start a fuzzy search in the frozen list
+							["<C-space>"] = actions.to_fuzzy_refine,
+						},
+					},
+					-- ... also accepts theme settings, for example:
+					-- theme = "dropdown", -- use dropdown theme
+					-- theme = { }, -- use own theme spec
+					-- layout_config = { mirror=true }, -- mirror preview pane
+				},
+			},
+		})
 
-    telescope.load_extension("fzf")
-    telescope.load_extension("live_grep_args")
+		telescope.load_extension("fzf")
+		telescope.load_extension("live_grep_args")
 
-    -- set keymaps
-    vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    vim.keymap.set(
-      "n",
-      "<leader>fg",
-      ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
-      { desc = "Find using ripgrep" }
-    )
-    vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Resume fuzzy find" })
-    vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    vim.keymap.set(
-      "n",
-      "<leader>fb",
-      "<cmd>Telescope buffers sort_mru=true sort_lastused=true initial_mode=normal theme=ivy<cr>",
-      { desc = "Open buffers" }
-    )
-    vim.keymap.set(
-      "n",
-      "<leader>fc",
-      "<cmd>Telescope grep_string<cr>",
-      { desc = "Find string under cursor in cwd" }
-    )
-    vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help" })
-    vim.keymap.set("n", "<leader>fq", "<cmd>Telescope registers<cr>", { desc = "Open registers" })
+		-- set keymaps
+		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		vim.keymap.set(
+			"n",
+			"<leader>fg",
+			":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+			{ desc = "Find using ripgrep" }
+		)
+		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope resume<cr>", { desc = "Resume fuzzy find" })
+		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		vim.keymap.set(
+			"n",
+			"<leader>fb",
+			"<cmd>Telescope buffers sort_mru=true sort_lastused=true initial_mode=normal theme=ivy<cr>",
+			{ desc = "Open buffers" }
+		)
+		vim.keymap.set(
+			"n",
+			"<leader>fc",
+			"<cmd>Telescope grep_string<cr>",
+			{ desc = "Find string under cursor in cwd" }
+		)
+		vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help" })
+		vim.keymap.set("n", "<leader>fq", "<cmd>Telescope registers<cr>", { desc = "Open registers" })
 
-    vim.keymap.set("n", "<leader>fd", function()
-      require("telescope.builtin").live_grep({
-        prompt_title = "Search Neovim tips and tricks",
-        cwd = "~/notes/neovim/",
-      })
-    end, { desc = "Search Neovim tips and tricks" })
-  end,
+		vim.keymap.set("n", "<leader>fd", function()
+			require("telescope.builtin").live_grep({
+				prompt_title = "Search Neovim tips and tricks",
+				cwd = "~/notes/neovim/",
+			})
+		end, { desc = "Search Neovim tips and tricks" })
+	end,
 }
